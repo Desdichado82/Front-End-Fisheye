b735214f75cf38c47fb34154ab37115c2c388091
David Mclean <mclean_1982@hotmail.com>
2023-05-29 13:24:26 +0200

    first commit

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,42 @@
+/*This is a JavaScript function named photographerFactory that takes an object data as an argument. 
+The function uses destructuring to extract the name and portrait properties from the data object. 
+It then creates a variable picture that stores the path to the portrait image.
+
+The function also defines an inner function named getUserCardDOM, 
+which creates an HTML article element and appends an img element and an h2 element to it. 
+The img element’s src attribute is set to the value of the picture variable, 
+and the text content of the h2 element is set to the value of the name variable. 
+The inner function returns the created article element.
+
+Finally, the photographerFactory function returns an object containing the properties name, picture, and getUserCardDOM.
+The value of the getUserCardDOM property is a reference to the inner function. */
+
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait ,city,country,tagline,price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const location = document.createElement('p');
+        const tagline_tag = document.createElement('p');
+        const price_tag = document.createElement('p');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
         h2.textContent = name;
+        location.textContent = `${country} , ${city}`;
+        location.classList.add('location');
+        tagline_tag.textContent = tagline;
+        tagline_tag.classList.add('tagline');
+        price_tag.textContent = `${price}€/jour'`;
+        price_tag.classList.add('price');
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tagline_tag);
+        article.appendChild(price_tag);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,115 @@
+    
+
+
+    
+    /*The spread operator (...) is a JavaScript expression that allows an iterable such as an array or
+     a string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected.
+      It can also be used to expand an object in places where zero or more key-value pairs (for object literals) are expected.
+
+    In the code you provided, the spread operator is used to create a new array by spreading the original photographers array three times:
+    [...photographers, ...photographers, ...photographers]. 
+    This creates a new array that contains three copies of the original photographers array. */
+    
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+        let data;
+        try {
+          // Fetch data from the JSON file
+          const response = await fetch("./data/photographers.json", {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
+      
+          // Check if the response status is not ok and throw an error
+          if (!response.ok) {
+            throw new Error(`An error occurred: ${response.status}`);
+          }
+      
+          // Parse the response data as JSON
+          data = await response.json();
+          console.log('Data returned by response.json():', data);
+        } catch (error) {
+          console.log(error);
+        }
+      
+        // Return the data once it has been fetched
+        return data;
+      }
 
+    /**
+     This is an async JavaScript function named displayData that takes an array of photographers as an argument. 
+     The function uses the querySelector method to select the first element in the document with the class name 
+     photographer_section and assigns it to the variable photographersSection.
+
+    The function then uses the forEach method to iterate over each photographer in the photographers array. 
+    For each photographer, the function calls the previously defined photographerFactory function and passes it 
+    the current photographer object. The photographerFactory function returns an object that contains a method named getUserCardDOM. 
+    This method is called and its return value (an HTML article element) is assigned to the variable userCardDOM.
+
+    The function then uses the appendChild method to append the userCardDOM element to the photographersSection element.
+     */
     async function displayData(photographers) {
+        console.log('Data passed to displayData:', photographers);
         const photographersSection = document.querySelector(".photographer_section");
-
+    
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
-    };
+    }
+
+    /*
+    This is an async JavaScript function named init. 
+    The function uses the await keyword to call the previously defined getPhotographers 
+    function and wait for it to complete. The getPhotographers function returns an object
+    with a property named photographers, which is destructured and assigned to a variable with the same name.
+
+    The function then calls the previously defined displayData function and passes it the photographers array as an argument.
+
+    After the init function is defined, it is immediately called.
+    */
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const data = await getPhotographers();
+        console.log('Data returned by getPhotographers:', data);
+        const photographers = data.photographers;
+        //const media = data.media;
         displayData(photographers);
-    };
+        
+    }
     
     init();
+
+
+    /*
+    USING Fetch !!
+    fetch is a JavaScript function that allows you to make network requests to retrieve resources, 
+    such as JSON data from a file. Here’s an example that shows how to use fetch to retrieve data from a JSON file:
+    
+            async function getData() {
+        try {
+            const response = await fetch('<URL>');
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+        }
+
+        getData();
+
+    In this example, the getData function uses fetch to make a request to the specified URL.
+    The await keyword is used to wait for the request to complete and the response to be returned.
+    The response.json() method is then used to parse the JSON data from the response.
+
+    The try...catch statement is used to handle any errors that may occur during the request. 
+    If an error occurs, it is logged to the console using console.error.
+
+    Once the data has been retrieved and parsed, it can be accessed and used in your code.
+
+    */
     
