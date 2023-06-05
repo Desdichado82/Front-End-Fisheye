c2403a281b1422068180fb99419c48bf93f870b4
David Mclean <mclean_1982@hotmail.com>
2023-06-05 15:29:17 +0200

    changes

---
+++ scripts/factories/mediagallery.js
@@ -0,0 +1,12 @@
+function mediaFactory(data){
+    const {id,photographerId,title,image,video,likes,date,price} = data
+
+    function getImage(){
+
+    }
+
+    function getVideo(){
+        
+    }
+}
+
