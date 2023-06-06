ac4112e269c5bddbaf06c9f78d62d3a857d3e56f
David Mclean <mclean_1982@hotmail.com>
2023-06-06 14:32:08 +0200

    changes today

--- scripts/factories/mediagallery.js
+++ scripts/factories/mediagallery.js
@@ -1,41 +1,65 @@
+// This function takes in an array of `media` objects as an argument and returns a `gallery` element
 function mediaFactory(media) {
+    // Log a message to the console indicating that the function was called with the specified `media` array
     console.log('mediaFactory called with media:', media);
+    
+    // Get a reference to the `main` element on the page
     const main = document.getElementById('main');
+    
+    // Log a message to the console indicating the `main` element that was found
     console.log('main element:', main);
+    
+    // Create a new `div` element to represent the gallery
     const gallery = document.createElement('div');
+    
+    // Set the `className` of the `gallery` element to "gallery"
     gallery.className = 'gallery';
-  
+    
+    // Loop through each item in the `media` array
     media.forEach((item) => {
+      // Log a message to the console indicating that the function is processing the current `item`
       console.log('processing media item:', item);
-      let mediaElement;
-      const article = document.createElement('article');
-      if (item.image) {
-        console.log('creating img element for image:', item.image);
-        mediaElement = document.createElement('img');
-        mediaElement.setAttribute("src", `assets/images/${item.image}`);
-        article.appendChild(mediaElement);
       
+      // Declare a variable to hold the media element (either an `img` or `video` element)
+      let mediaElement;
+      
+      // Create a new `article` element to represent the current media item
+      const article = document.createElement('article');
+      
+      // Check if the current `item` has an `image` property
+      if (item.image) {
+        // If it does, create an `img` element using the `createImageElement` function
+        mediaElement = createImageElement(item);
+        
+        // Append the `img` element to the `article` element
+        article.appendChild(mediaElement);
       } else if (item.video) {
-        console.log('creating video element for video:', item.video);
-        mediaElement = document.createElement('video');
-        mediaSource = document.createElement('source');
-        mediaElement.setAttribute("src", `assets/images/${item.video}`);
-        mediaSource.setAttribute("src", `assets/images/${item.video}`);
-        mediaSource.setAttribute("type", `video/mp4`);
-        mediaElement.appendChild(mediaSource);
+        // If the current `item` has a `video` property, create a `video` element using the `createVideoElement` function
+        mediaElement = createVideoElement(item);
+        
+        // Append the `video` element to the `article` element
         article.appendChild(mediaElement);
       }
+      
+      // Check if a media element was created (either an `img` or `video` element)
       if (mediaElement) {
-        // add an onclick event Listener 
+        // If it was, add an onclick event listener to the media element
         mediaElement.onclick = () => {
-          //implement lightbox functionality here
+          // When the media element is clicked, implement lightbox functionality here
           //lightbox();
         };
+        
+        // Append the `article` element to the `gallery` element
         gallery.appendChild(article);
       }
     });
+    
+    // Append the `gallery` element to the `main` element on the page
     main.appendChild(gallery);
+    
+    // Return the `gallery` element
     return gallery;
-}
+  }
   
-
+   
+   
--- scripts/utils/videoCreator.js
+++ scripts/utils/videoCreator.js
@@ -1,11 +1,28 @@
+// This function takes in an `item` object as an argument and returns a `video` element
 function createVideoElement(item) {
+    // Log a message to the console indicating that the function is creating a `video` element for the specified video
     console.log('creating video element for video:', item.video);
+    
+    // Create a new `video` element
     const mediaElement = document.createElement('video');
+    
+    // Create a new `source` element
     const mediaSource = document.createElement('source');
+    
+    // Set the `src` attribute of the `video` element to the path of the video file on the server
     mediaElement.setAttribute("src", `assets/images/${item.video}`);
+    
+    // Set the `src` attribute of the `source` element to the path of the video file on the server
     mediaSource.setAttribute("src", `assets/images/${item.video}`);
+    
+    // Set the `type` attribute of the `source` element to specify that the video file is in MP4 format
     mediaSource.setAttribute("type", `video/mp4`);
+    
+    // Append the `source` element to the `video` element
     mediaElement.appendChild(mediaSource);
+    
+    // Return the `video` element
     return mediaElement;
   }
+  
   
