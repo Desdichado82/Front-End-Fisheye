7360e1a1938d70d3205493ac8e35ec181161d373
David Mclean <mclean_1982@hotmail.com>
2023-06-14 13:28:49 +0200

    accesibility update

--- scripts/factories/mediagallery.js
+++ scripts/factories/mediagallery.js
@@ -1,53 +1,69 @@
 // This function takes in an array of `media` objects as an argument and returns a `gallery` element
 function mediaFactory(media) {
-
   console.log('mediaFactory called with media:', media);
-    
-    const main = document.getElementById('main');
-    console.log('main element:', main);
 
-    // clear existing gallery 
+  const main = document.getElementById('main');
+  console.log('main element:', main);
+
+  // clear existing gallery
   const existingGallery = main.querySelector('.gallery');
   if (existingGallery) {
     main.removeChild(existingGallery);
   }
 
-    const gallery = document.createElement('div');
-    gallery.className = 'gallery';
-    
-    // Loop through each item in the `media` array
-    media.forEach((item) => {
-      // Log a message to the console indicating that the function is processing the current `item`
-      console.log('processing media item:', item);
-      
-      // Declare a variable to hold the media element (either an `img` or `video` element)
-      let mediaElement;
-      
-      const article = document.createElement('article');
-      
-      // Check if the current `item` has an `image` property
-      if (item.image) {
-        // If it does, create an `img` element using the `createImageElement` function
-        mediaElement = createImageElement(item);
-        article.appendChild(mediaElement);
-      } else if (item.video) {
-        // If the current `item` has a `video` property, create a `video` element using the `createVideoElement` function
-        mediaElement = createVideoElement(item);
-        article.appendChild(mediaElement);
-      }
-      
-      // Check if a media element was created (either an `img` or `video` element)
+  const gallery = document.createElement('div');
+  gallery.className = 'gallery';
+
+  // Loop through each item in the `media` array
+  media.forEach((item) => {
+    // Log a message to the console indicating that the function is processing the current `item`
+    console.log('processing media item:', item);
+
+    // Declare a variable to hold the media element (either an `img` or `video` element)
+    let mediaElement;
+
+    const article = document.createElement('article');
+    const infoContainer = document.createElement('div');
+    const title = document.createElement('p');
+    const likeContainer = document.createElement('div');
+    const likeCounter = document.createElement('span');
+    const likeBtn = document.createElement('button');
+
+    infoContainer.classList.add('infoContainer');
+    likeContainer.classList.add('likeContainer');
+
+    likeBtn.setAttribute('aria-label', 'Like button');
+
+    // Check if the current `item` has an `image` property
+    if (item.image) {
+      // If it does, create an `img` element using the `createImageElement` function
+      mediaElement = createImageElement(item);
+      mediaElement.setAttribute('alt', item.title);
+      article.appendChild(mediaElement);
+    } else if (item.video) {
+      // If the current `item` has a `video` property, create a `video` element using the `createVideoElement` function
+      mediaElement = createVideoElement(item);
+      mediaElement.setAttribute('alt', item.title);
+      article.appendChild(mediaElement);
+    }
+
+    // Check if a media element was created (either an `img` or `video` element)
     if (mediaElement) {
       mediaElement.onclick = () => {
         openLightbox(media, item);
       };
+
+      title.textContent = item.title;
+      likeCounter.textContent = item.likes;
+      infoContainer.appendChild(title);
+      infoContainer.appendChild(likeContainer);
+      likeContainer.appendChild(likeCounter);
+      likeContainer.appendChild(likeBtn);
+      article.appendChild(infoContainer);
       gallery.appendChild(article);
     }
   });
   main.appendChild(gallery);
 
   return gallery;
-  }
-  
-   
-   
+}
--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,78 +1,81 @@
-
 // This function takes in a `data` object as an argument and returns an object with two methods: `getUserCardDOM` and `displayProfile`
 function photographerFactory(data) {
   // Destructure the `data` object to extract the properties we need
-    const { name, id, portrait, city, country, tagline, price } = data;
-   // This function returns a DOM element representing the user card for the photographer
-    function getUserCardDOM() {
-       // Create the necessary DOM elements
-      const article = document.createElement('article');
-      const link = document.createElement('a');
-      const img = document.createElement('img');
-      const h2 = document.createElement('h2');
-      const location = document.createElement('p');
-      const tagline_tag = document.createElement('p');
-      const price_tag = document.createElement('p');
-  // Create a new URLSearchParams object to store the query parameters for the link
-      const params = new URLSearchParams();
-        // Append the `id` parameter to the URLSearchParams object
-      params.append("id", id);
-        // Set the `href` attribute of the `link` element to point to the photographer page with the specified query parameters
-      link.setAttribute("href", `photographer.html?${params.toString()}`);
-       // Set the `title` attribute of the `link` element to the photographer's name
-      link.setAttribute("title", name);
-       // Set the `src` attribute of the `img` element to point to the photographer's portrait image on the server
-      img.setAttribute("src", `assets/photographers/${portrait}`);
-      // Set the `alt` attribute of the `img` element to the photographer's name
-      img.setAttribute("alt", name);
-       // Set the text content of the `h2` element to the photographer's name
-      h2.textContent = name;
-        
+  const { name, id, portrait, city, country, tagline, price } = data;
+
+  // This function returns a DOM element representing the user card for the photographer
+  function getUserCardDOM() {
+    // Create the necessary DOM elements
+    const article = document.createElement('article');
+    const link = document.createElement('a');
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const location = document.createElement('p');
+    const tagline_tag = document.createElement('p');
+    const price_tag = document.createElement('p');
+
+    link.setAttribute('aria-label', 'Link');
+
+    // Create a new URLSearchParams object to store the query parameters for the link
+    const params = new URLSearchParams();
+    // Append the `id` parameter to the URLSearchParams object
+    params.append("id", id);
+    // Set the `href` attribute of the `link` element to point to the photographer page with the specified query parameters
+    link.setAttribute("href", `photographer.html?${params.toString()}`);
+    // Set the `title` attribute of the `link` element to the photographer's name
+    link.setAttribute("title", name);
+    // Set the `src` attribute of the `img` element to point to the photographer's portrait image on the server
+    img.setAttribute("src", `assets/photographers/${portrait}`);
+    // Set the `alt` attribute of the `img` element to an empty string
+    img.setAttribute("alt", "");
+    // Set the text content of the `h2` element to the photographer's name
+    h2.textContent = name;
+
     // Set the text content of the `location` element to the photographer's city and country
-      location.textContent = `${country} , ${city}`;
-      location.classList.add('location');
-      tagline_tag.textContent = tagline;
-      tagline_tag.classList.add('tagline');
-      price_tag.textContent = `${price}€/jour'`;
-      price_tag.classList.add('price');
-      
-   // Append all of these elements together in a hierarchy that represents a user card for this photographer
-      link.appendChild(img);
-      article.appendChild(link);
-      article.appendChild(h2);
-      article.appendChild(location);
-      article.appendChild(tagline_tag);
-      article.appendChild(price_tag);
+    location.textContent = `${country} , ${city}`;
+    location.classList.add('location');
+    tagline_tag.textContent = tagline;
+    tagline_tag.classList.add('tagline');
+    price_tag.textContent = `${price}€/jour'`;
+    price_tag.classList.add('price');
+
+    // Append all of these elements together in a hierarchy that represents a user card for this photographer
+    link.appendChild(img);
+    article.appendChild(link);
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(tagline_tag);
+    article.appendChild(price_tag);
+
+    return article;
+  }
+  function displayProfile() {
+    const photographersHeader = document.querySelector('.photograph-header');
+    const info = document.createElement('div');
+    const picture = `assets/photographers/${portrait}`;
+    const profile = document.createElement('img');
+    const nameTag = document.createElement('h2');
+    const address = `${city}, ${country}`;
+    const locationTag = document.createElement('p');
+    const taglineTag = document.createElement('p');
   
-      return article;
-    }
+    profile.setAttribute('src', picture);
+    // Set the `alt` attribute of the `profile` element to an empty string
+    profile.setAttribute('alt', '');
   
-    function displayProfile() {
-      const photographersHeader = document.querySelector('.photograph-header');
-      const info = document.createElement('div');
-      const picture = `assets/photographers/${portrait}`;
-      const profile = document.createElement('img');
-      const nameTag = document.createElement('h2');
-      const address = `${city}, ${country}`;
-      const locationTag = document.createElement('p');
-      const taglineTag = document.createElement('p');
-      
-      profile.setAttribute('src', picture);
+    nameTag.textContent = name;
+    locationTag.textContent = address;
+    taglineTag.textContent = tagline;
   
-      nameTag.textContent = name;
-      locationTag.textContent = address;
-      taglineTag.textContent = tagline;
-      
-      locationTag.className = 'location';
+    locationTag.className = 'location';
   
-      photographersHeader.appendChild(info);
-      photographersHeader.appendChild(profile);
-      
-      info.appendChild(nameTag);
-      info.appendChild(locationTag);
-      info.appendChild(taglineTag);
-    }
+    photographersHeader.appendChild(info);
+    photographersHeader.appendChild(profile);
   
-    return {getUserCardDOM, displayProfile };
+    info.appendChild(nameTag);
+    info.appendChild(locationTag);
+    info.appendChild(taglineTag);
   }
   
+  return { getUserCardDOM, displayProfile };
+  }
